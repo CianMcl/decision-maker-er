b0c62d4a58f73292c43719e2add3be55abf3f264
CianM <cmcl0022@student.monash.edu>
2022-12-15 12:13:03 +1100

    really just testing out swing here. this is bad code, but I'm learning

--- src/Startup.java
+++ src/Startup.java
@@ -3,9 +3,6 @@ package src;
 public class Startup{
     public static void main(String[] args){
         ViewControl vc = new ViewControl();
-        vc.openFrame();
-
+        vc.openFrame(vc.panelCreation());
     }
-
-
 }
--- src/ViewControl.java
+++ src/ViewControl.java
@@ -1,22 +1,28 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ViewControl implements ActionListener {
 
     static JFrame frame;
-    static JButton button;
+    static JButton button, diffButton;
     static JPanel panel;
 
 
-    public void openFrame(){
+    public void openFrame(JPanel insert){
         frame = new JFrame();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(100,100,800,800);
+        frame.setMinimumSize(new Dimension(800,800));
+        //frame.setBounds(100,100,800,800);
+        frame.add(insert);
 
+    }
+
+    public JPanel panelCreation(){
         panel = new JPanel();
 
         button = new JButton("Click");
@@ -24,14 +30,29 @@ public class ViewControl implements ActionListener {
 
         panel.add(button);
 
-        frame.add(panel);
+        return panel;
+
+    }
+
+    public JPanel diffPanel(){
+        panel = new JPanel();
+
+        diffButton = new JButton("This is now different");
+        diffButton.addActionListener(new ViewControl());
+
+        panel.add(diffButton);
+
+        return panel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button){
             JOptionPane.showMessageDialog(null, "Button!");
-
+            openFrame(diffPanel());
+        } else if (e.getSource() == diffButton){
+            String iono = JOptionPane.showInputDialog("Please enter?");
+            System.out.println(iono);
         }
     }
 
