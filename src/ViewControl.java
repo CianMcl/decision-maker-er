dd1a07655c3fd9a13afe909cea95c072eeddd3a7
CianM <cmcl0022@student.monash.edu>
2022-12-15 14:17:56 +1100

    testing new components, splash screen is reasonable
    also fixed but regarding open - set visible has to go at the end of frame.

--- src/ViewControl.java
+++ src/ViewControl.java
@@ -11,19 +11,22 @@ public class ViewControl implements ActionListener {
     static JButton button;
     static JPanel panel;
     static JLabel heading;
-
+    static JEditorPane box;
 
 
     public void openFrame(){
         frame = new JFrame();
-        frame.setVisible(true);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setMinimumSize(new Dimension(800,800));
         //frame.setBounds(100,100,800,800);
 
         panel = defaultScreen();
+
         frame.add(panel);
 
+        frame.setVisible(true);
     }
 
 
@@ -31,18 +34,33 @@ public class ViewControl implements ActionListener {
         panel = new JPanel(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        heading = new JLabel("Welcome to the decision maker.");
+        heading = new JLabel("Welcome to the Decision Maker.");
+        heading.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
 
-        button = new JButton("Click");
+        box = new JEditorPane();
+        box.setContentType("text/plain");
+        box.setText("Welcome to the decision maker.\nThis will make things " +
+                "simple, as long as you stick with my logic. I promise that" +
+                " it won't be unreasonable.");
+        box.setBounds(new Rectangle(400, 800));
+        box.setEditable(false);
+
+        button = new JButton("Click here to start!");
         button.addActionListener(new ViewControl());
 
         c.gridx = 0;
         c.gridy = 0;
-        c.weightx = 1;
+        c.weighty = 1;
         panel.add(heading, c);
 
         c.gridx = 0;
         c.gridy = 1;
+        c.weighty = 1;
+        panel.add(box, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weighty = 1;
         panel.add(button, c);
 
         return panel;
@@ -54,7 +72,6 @@ public class ViewControl implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
             JOptionPane.showMessageDialog(null, "Button!");
-
         }
     }
 }
